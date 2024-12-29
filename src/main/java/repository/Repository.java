25c89f987e482ae077e6998ed66c0ea54fe38e84
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2024-12-29 22:18:38 +0100

    Fix: deleteExpense method return boolean in Repository interface

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 public interface Repository {
     boolean addExpense(Expense expense);
     Expense updateExpense(Expense expense);
-    void deleteExpense(Expense expense);
+    boolean deleteExpense(Expense expense);
     Expense getExpenseById(int id);
     ArrayList<Expense> getAllExpenses();
 }
