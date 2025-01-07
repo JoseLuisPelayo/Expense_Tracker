bf8e27977990921cac111d5cad4e7fec623ff0f3
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-01-07 21:45:35 +0100

    Refactor: Bat file to compile the project and add the folder at PATH in windows updated

--- setup.bat
+++ setup.bat
@@ -2,35 +2,20 @@
 REM Mensaje de inicio
 echo === Iniciando la construccion del proyecto ===
 
-REM Navegar al directorio del proyecto (opcional)
 cd /d %~dp0
-
-REM Compilar el proyecto con Maven
-
-echo === Ejecutando 'mvn clean install' ===
+echo === Running 'mvn clean install' ===
 call mvn clean install
-pause
 
 if errorlevel 1 (
     echo Error durante la construcción del proyecto. Verifica los logs.
     pause
-    exit /b
+    exit
 )
 
-REM Ruta donde está el archivo jar y el bat
-pause
-set "APP_DIR=%CD%"
-pause
-REM Añadir el directorio al PATH del sistema de forma permanente
+set APP_DIR=%~dp0target
+
 echo === Añadiendo el directorio actual al PATH ===
-setx PATH "%PATH%;%APP_DIR%" /M
-if errorlevel 1 (
-    echo No se pudo añadir el directorio al PATH. Puede que no tengas permisos de administrador.
-    pause
-    exit /b
-)
+setx PATH "%PATH%;%APP_DIR%"
 
-echo El directorio %APP_DIR% se ha añadido al PATH del sistema.
-echo Ahora puedes usar 'xpense' desde cualquier ubicación.
-
-pause
+copy .\expense.bat %APP_DIR%\
+cmd
