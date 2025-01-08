ed430e1d2ef454b7565c1d75725cc801c1e4b12a
JoseLuisPelayo <joseluis.garcia893@comunidadunir.net>
2025-01-08 16:08:33 +0100

    docs: JavaDoc comments for JsonManager class added

--- src/main/java/utils/JsonManager.java
+++ src/main/java/utils/JsonManager.java
@@ -7,7 +7,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class provides the implementation for the IJsonManager interface.
+ * It provides methods to convert JSON data to a list of objects and
+ * convert a list of objects to JSON data
+ */
 public class JsonManager implements IJsonManager{
+
+    /**
+     * Converts JSON data from a file to a List of objects of the specifies class.
+     *
+     * @param file the file containeng the JSON data
+     * @param <T> the type of the objects in the list
+     * @param myClass the class of the objects in the list
+     * @return return a list of objects of the specified class or an empty list if an error occurs or the file is empty
+     */
     public <T> List<T> jsonDataToArrayList(File file, Class<T> myClass) {
         try {
         ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
@@ -19,22 +33,27 @@ public class JsonManager implements IJsonManager{
         return mapper.readValue(file, mapper.getTypeFactory().constructCollectionType(ArrayList.class ,myClass));
 
         } catch (IOException e) {
-            System.err.println("Error al leer el archivo JSON: " + e.getMessage());
+            System.err.println("Error reading JSON file: " + e.getMessage());
             return new ArrayList<>();
         }
     }
 
+    /**
+     * Converts a list of objects to JSON data and writes it into a file.
+     *
+     * @param file the file to write the JSON data
+     * @param listClass the list of objects to convert to JSON data
+     * @return true if the file was successfully written to the file, false otherwise
+     * @throws IOException if an I/O error occurs
+     */
     public boolean listToJson(File file, ArrayList listClass) throws IOException {
         try {
             ObjectMapper mapper = new ObjectMapper().findAndRegisterModules();
             mapper.writeValue(file, listClass);
             return true;
         } catch (IOException e) {
-            System.err.println("Error al escribir el archivo JSON: " + e.getMessage());
+            System.err.println("Error writing JSON file: " + e.getMessage());
             return false;
         }
     }
-
-
-
 }
